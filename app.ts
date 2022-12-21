86208ed62dc66b9c7913c2ea2d246c7eaba37c58
steffen.jaser <steffen.jaser@aracom.de>
2022-12-21 14:26:23 +0100

    added strict query

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
-import { connect } from "mongoose";
+import mongoose, { connect } from "mongoose";
 import { User } from "./models/User";
+
+mongoose.set("strictQuery", false);
 
 run().catch((err) => console.log(err));
 
 async function run() {
   // 4. Connect to MongoDB
   await connect("mongodb://localhost:27017/tsapp");
+
+  console.log("Connected to DB.");
 
   const newUser = new User({
     name: "Testname",
